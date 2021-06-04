30ebaaebca6e017aaa04bfbbb88f03d1f42e6708
anupam2020 <63058877+anupam2020@users.noreply.github.com>
2021-06-04 18:51:31 +0530

    Alert Message Changed

--- app/src/main/java/com/example/sharedprefs_user_reg/MainActivity.java
+++ app/src/main/java/com/example/sharedprefs_user_reg/MainActivity.java
@@ -133,7 +133,7 @@ public class MainActivity extends AppCompatActivity {
 
         AlertDialog.Builder builder=new AlertDialog.Builder(this);
         builder.setTitle("Exit");
-        builder.setMessage("Are you sure want to exit?");
+        builder.setMessage("Do you really want to exit?");
         builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
